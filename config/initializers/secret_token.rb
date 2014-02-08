7aad6ad9f4949cd41b45e9ffcc40c678ba5d010d
David Bella <dbella1@gmail.com>
2014-02-08 13:10:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DayOneHack::Application.config.secret_key_base = '9b9e6652acc218886e196f43fca6f0caf3cd9e9bdf63e6729c1da0d66414daaf10185b3afc3ddf869aaf07a04189a2141eeba35816360947bb47a428f0716156'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DayOneHack::Application.config.session_store :cookie_store, key: '_day-one-hack_session'
