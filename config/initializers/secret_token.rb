50951db6ca97a536f4413cc79e5a3d9eeb29f8a0
tiffanypeon <tiffanypeon@gmail.com>
2014-02-08 12:54:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DayOne::Application.config.secret_key_base = '21d62bf3ee44fdbb8b0cd653b25a10172b51cfda168d61c0b68c9de242f2fcdc1841a4f8ba00fcf9be8ec7b95b1e73e9761693abc9e3b53ce8000db1c95cd117'
