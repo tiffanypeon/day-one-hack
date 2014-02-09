d15210afe68fc0932246b257b661cf0a11f6ee8b
David Bella <dbella1@gmail.com>
2014-02-09 09:10:22 -0500

    Created simple controller to display list of possible accounts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ DayOneHack::Application.routes.draw do
 
   resources :events
 
+  get 'accounts/' => 'accounts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
